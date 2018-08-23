6587ad504fed484d7d197365634e4f0fdbdafdbf
Mark Goadrich <goadrich@hendrix.edu>
2018-08-23 09:59:11 -0500

    Adding Par loop for parallel execution

--- Simulator/Fujisan/Program.cs
+++ Simulator/Fujisan/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Fujisan
 {
@@ -10,6 +13,9 @@ namespace Fujisan
         {
             int TRIALS = 10;
             int EXP = 50;
+            Setup setup = Setup.RANDOM;
+
+            Random random = new Random();
 
             // Easy output for copying into a spreadsheet
             Console.WriteLine("solved\tdead\tavelen");
@@ -21,71 +27,78 @@ namespace Fujisan
                 int count = 0;
                 int lensum = 0;
                 int dead = 0;
-                Random random = new Random();
 
-                for (int i = 0; i < EXP; i++)
-                {
-                    // Store all boards seen in found
-                    HashSet<Board> found = new HashSet<Board>();
+                Parallel.For(0, EXP,
+                   i =>
+                   {
+                       // Store all boards seen in found
+                       HashSet<Board> found = new HashSet<Board>();
 
-                    // Keep track of board states to explore in frontier
-                    // Sort them by heuristic plus current path length for A*
-                    SortedList<double, Board> frontier = new SortedList<double, Board>();
+                       // Keep track of board states to explore in frontier
+                       // Sort them by heuristic plus current path length for A*
+                       SortedList<double, Board> frontier = new SortedList<double, Board>();
 
-                    // Create a new board and place it in the frontier
-                    Board start = new Board(random);
-                    Debug.WriteLine("Starting:");
-                    Debug.WriteLine(start + "\n");
-                    frontier.Add(start.length + start.Heuristic() + (0.1 * random.NextDouble()), start);
+                       // Create a new board and place it in the frontier
+                       Board start = new Board(random, setup);
+                       Debug.WriteLine(start);
+                       Debug.WriteLine("Starting:");
+                       Debug.WriteLine(start + "\n");
+                       frontier.Add(start.length + start.Heuristic() + (0.1 * random.NextDouble()), start);
 
-                    // Keep searching the frontier until it is empty or
-                    // a solution is found
-                    while (frontier.Count > 0)
-                    {
+                       // Keep searching the frontier until it is empty or
+                       // a solution is found
+                       while (frontier.Count > 0)
+                       {
 
-                        // Take the most promising board state, remove from
-                        // frontier and add it to the found set
-                        var keys = frontier.Keys;
-                        var first = keys[0];
-                        Board board = frontier[first];
-                        frontier.Remove(first);
-                        found.Add(board);
+                           // Take the most promising board state, remove from
+                           // frontier and add it to the found set
+                           var keys = frontier.Keys;
+                           var first = keys[0];
+                           Board board = frontier[first];
+                           frontier.Remove(first);
+                           found.Add(board);
 
-                        // Find the children of the current board
-                        List<Board> stuff = board.GetChildren();
-                        foreach (Board b in stuff)
-                        {
-                            // Did you find a solution?
-                            if (b.Solved())
-                            {
-                                // Yay! Record statistics
-                                Debug.WriteLine("SOLUTION!!!!");
-                                Debug.WriteLine(b.Path());
-                                lensum += b.length;
-                                frontier = new SortedList<double, Board>();
-                                count++;
-                                break;
-                            }
+                           // Find the children of the current board
+                           List<Board> stuff = board.GetChildren();
+                           foreach (Board b in stuff)
+                           {
+                               // Did you find a solution?
+                               if (b.Solved())
+                               {
+                                   // Yay! Record statistics
+                                   Debug.WriteLine("SOLUTION!!!!");
+                                   Debug.WriteLine(b.Path());
+                                   frontier.Clear();
+                                lock (random)
+                                   {
+                                       lensum += b.length;
+                                       count++;
+                                   }
+                                   break;
+                               }
 
-                            // If you have never seen this board before
-                            // Add it to the frontier
-                            if (!found.Contains(b) && !frontier.ContainsValue(b))
-                            {
-                                frontier.Add(b.length + b.Heuristic() + (0.1 * random.NextDouble()), b);
-                            }
-                            else
-                            {
-                                //Console.WriteLine("WOAH!");
-                            }
-                        }
-                    }
+                               // If you have never seen this board before
+                               // Add it to the frontier
+                               if (!found.Contains(b) && !frontier.ContainsValue(b))
+                               {
+                                   frontier.Add(b.length + b.Heuristic() + (0.1 * random.NextDouble()), b);
+                               }
+                               else
+                               {
+                                   //Console.WriteLine("WOAH!");
+                               }
+                           }
+                       }
 
-                    // Record when no children of initial state could be found
-                    if (found.Count == 1)
-                    {
-                        dead++;
-                    }
-                }
+                       // Record when no children of initial state could be found
+                       if (found.Count == 1)
+                       {
+                        lock (random)
+                           {
+                               dead++;
+                           }
+                       }
+                   });
 
                 Console.WriteLine(((float)count / EXP) +
                                   "\t" + ((float)dead / EXP) +
